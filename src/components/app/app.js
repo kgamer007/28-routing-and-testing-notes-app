3d609dd43062f5a7a288a69fe9232e2a7c298b8a
Kevin Hwangpo <hwangpo3@gmail.com>
2018-07-18 13:02:55 -0700

    scaffolding kind of completed

---
+++ src/components/app/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import './app/scss';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <BrowserRouter>
+          <div>
+            <header>
+                <h1> Budget Tracker</h1>
+                  <nav> 
+                      <ul> 
+                        <ul
+    )
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render as renderDom } from 'react-dom';
+import './style.main.scss';
+
+const root = document.createElement('div');
+document
